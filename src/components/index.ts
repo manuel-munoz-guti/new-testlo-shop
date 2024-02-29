79f72affff7d47116aed56db71e08b07d86dea3a
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2024-02-28 23:32:37 -0400

    adding slideshow to product page

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,8 @@ export * from './products/product-grid/ProductGridItem';
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
+
+export * from './product/size-selector/SizeSelector';
+export * from './product/quantity-selector/QuantitySelector';
+export * from './product/slideshow/ProductSlideshow';
+export * from './product/slideshow/ProductMobileSlideshow';
