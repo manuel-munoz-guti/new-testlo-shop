bda330d22b604acefa79d3012a2eb43645cad697
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2024-03-18 21:50:45 -0400

    Adding prima

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { TopMenu } from "@/components";
+import { Footer, TopMenu } from "@/components";
 import { Sidebar } from "@/components/ui/sidebar/Sidebar";
 
 export default function ShopLayout({ children }: { children: React.ReactNode; }) {
@@ -9,6 +9,7 @@ export default function ShopLayout({ children }: { children: React.ReactNode; })
         <div className="px-0 sm:px-10">
         { children }
         </div>
+      <Footer />
     </main>
   );
 }
